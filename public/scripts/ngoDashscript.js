da14c9db4258c29439cd76712084cb8218a9e302
donovan1025 <S10257381@connect.np.edu.sg>
2024-06-30 16:43:38 +0800

    refactor: remove redundant code

--- public/scripts/ngoDashscript.js
+++ public/scripts/ngoDashscript.js
@@ -1,19 +1,4 @@
 //donovan
-/*static async getAllBooks() {
-    const connection = await sql.connect(dbConfig);
-
-    const sqlQuery = `SELECT * FROM Books`; // Replace with your actual table name
-
-    const request = connection.request();
-    const result = await request.query(sqlQuery);
-
-    connection.close();
-
-    return result.recordset.map(
-      (row) => new Book(row.id, row.title, row.author)
-    ); // Convert rows to Book objects
-  }
-    */
 
 //To format time (example: 05:30 PM)
 function formatTimeRange(startTimeString, endTimeString) {
