c127fc96d2dfc446db492c97f7e1f9b29a73bece
brazeenNP <s10255790@connect.np.edu.sg>
2024-07-03 17:34:14 +0800

    (PRACTICAL) feat: added registerUser method

--- practicals/PolytechnicLibraryAPI/controllers/usersController.js
+++ practicals/PolytechnicLibraryAPI/controllers/usersController.js
@@ -2,7 +2,7 @@ const User = require("../models/user")
 
 const getAllUsers = async (req, res) => {
     try {
-        const books = await User.getAllUsers()
+        const users = await User.getAllUsers()
         res.json(users)
     }
     catch(error) {
@@ -12,9 +12,23 @@ const getAllUsers = async (req, res) => {
 }
 
 const getUserById = async (req, res) => {
-    const userid = req.params.id;
+  const userId = req.params.id;
+  try {
+    const user = await User.getUserById(userId)
+    if (!user) {
+      return res.status(404).send("User not found")
+    }
+    res.json(user);
+  } catch (error) {
+    console.error(error);
+    res.status(500).send("Error retrieving user");
+  }
+};
+
+const getUserByUsername = async (req, res) => {
+    const username = req.params.username;
     try {
-      const book = await User.getUserById(userid)
+      const user = await User.getUserByUsername(username)
       if (!user) {
         return res.status(404).send("User not found")
       }
@@ -25,7 +39,42 @@ const getUserById = async (req, res) => {
     }
 };
 
+const createUser = async (req, res) => {
+  const newUser = req.body;
+  try {
+      const createdUser = await User.createUser(newUser)
+      res.status(201).json(createdUser)
+  }
+  catch(error) {
+      res.status(500).send("Error creating user")
+  }
+}
 
+async function registerUser(req, res) {
+  const { username, password, role } = req.body;
+
+  try {
+    // Validate user data
+    if (password.length < 5) {
+      return res.status(400).json({ message: "Password too short" });
+    }
+    // Check for existing username
+    const existingUser = await getUserByUsername(username);
+    if (existingUser) {
+      return res.status(400).json({ message: "Username already exists" });
+    }
+
+    // Hash password
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+    const newUser = {username: username, passwordHash: hashedPassword, role: role}
+    await createUser(newUser);
+    return res.status(201).json({ message: "User created successfully" });
+  } catch (err) {
+    console.error(err);
+    return res.status(500).json({ message: "Internal server error" });
+  }
+}
 
 
 
@@ -33,4 +82,7 @@ const getUserById = async (req, res) => {
 module.exports = {
     getAllUsers,
     getUserById,
+    getUserByUsername,
+    createUser,
+    registerUser
 }
--- practicals/PolytechnicLibraryAPI/models/user.js
+++ practicals/PolytechnicLibraryAPI/models/user.js
@@ -1,6 +1,5 @@
 const sql = require("mssql")
 const dbConfig = require("../dbConfig");
-const { user } = require("../../../dbConfig");
 
 class User {
     constructor(user_id, username, passwordHash, role) {
@@ -42,23 +41,40 @@ class User {
         //possible null
     }
 
-    static async updateUser(id, newUserData) {
+    static async getUserByUsername(username) {
+        const connection = await sql.connect(dbConfig);
+
+        const sqlQuery = `SELECT * FROM Users WHERE username = @username`; //params
+
+        const request = connection.request();
+        request.input("username", username)
+        const result = await request.query(sqlQuery);
+
+        connection.close();
+
+        return result.recordset[0] ?
+            new User(result.recordset[0].user_id, result.recordset[0].username, result.recordset[0].passwordHash, result.recordset[0].role)
+         : null //convert rows to NGOs
+        //possible null
+    }
+
+    static async createUser(newUserData) {
         const connection = await sql.connect(dbConfig)
 
-        const sqlQuery = `UPDATE Users SET username = @username, passwordHash = @passwordHash, role = @role WHERE user_id = @user_id`
+        const sqlQuery = `INSERT INTO Users (username, passwordHash, role) VALUES (@username, @passwordHash, @role); SELECT SCOPE_IDENTITY() AS user_id;`
 
         const request = connection.request()
-        request.input("user_id", id)
-        request.input("username", newUserData.username || null)
-        request.input("passwordHash", newUserData.passwordHash || null)
-        request.input("role", newUserData.role || null)
-        
+        request.input("username", newUserData.username)
+        request.input("passwordHash", newUserData.passwordHash)
+        request.input("role", newUserData.role)
 
-        await request.query(sqlQuery)
+
+        const result = await request.query(sqlQuery)
 
         connection.close()
 
-        return this.getUserById(id)
+        return this.getUserById(result.recordset[0].id)
+
     }
 }
 
